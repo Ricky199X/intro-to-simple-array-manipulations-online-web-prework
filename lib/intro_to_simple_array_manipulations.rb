ec74ba043e45cb1a40c2e5bfaaeae9f31f1133bf
Ricky Rojas <rickyrojas19@gmail.com>
2019-03-12 00:30:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,17 +3,14 @@ def using_push(array, string)
 end
 
 
-
 def using_unshift(array, string)
   array.unshift(string)
 end
 
 
-
 def using_pop(array)
   array.pop
 end
-
 
 
 def pop_with_args(array)
@@ -25,6 +22,34 @@ def using_shift(array)
   array.shift
 end
 
+
 def shift_with_args(array)
   array.shift(2)
 end
+
+
+def using_concat(array1, array)
+  array1.concat(array2)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
